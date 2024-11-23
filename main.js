8c332af998c0ccb03a9f9183c815db9029cfcbaa
omareldieb <126256198+omareldieb@users.noreply.github.com>
2024-11-23 22:16:51 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Example for smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
